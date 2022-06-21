1afa48d016eae43d469d76801f1b94e22d5e4e8c
Jaay06 <josekoyi9@gmail.com>
2022-06-21 20:35:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const name = 'Joseph Igbekoyi'
+const height = 175.5
+const country = 'Nigeria'
+
+console.log(`My name is ${name} i am from ${country}, I am ${height}cm tall`)
+alert(`My name is ${name} i am from ${country}, I am ${height}cm tall`)
